1f54c94d27405e5b06b34565a7652925b50803ed
dev <almas1610@gmail.com>
2025-07-25 08:27:24 +0500

    [Init]:: Initialize project for news portal

---
+++ app/pages/auth/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Auth page</h1>
+    <form @submit="toAuth">
+      <input type="text" placeholder="Login">
+      <input type="password" placeholder="Password">
+      <input type="submit">
+    </form>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const toAuth = (e: Event) => {
+  e.preventDefault();
+  console.log("auth...")
+}
+</script>
+
+<style></style>

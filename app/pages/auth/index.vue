6922de47d72e379d932626fb918022b5c27045f4
dev <almas1610@gmail.com>
2025-07-25 12:22:01 +0500

    [feature-ui]: get-ready auth page

--- app/pages/auth/index.vue
+++ app/pages/auth/index.vue
@@ -1,19 +1,37 @@
 <template>
-  <div>
-    <h1>Auth page</h1>
-    <form @submit="toAuth">
-      <input type="text" placeholder="Login">
-      <input type="password" placeholder="Password">
-      <input type="submit">
-    </form>
+  <div style="height: 100vh; box-shadow: var(--el-border-color-light) 0px 0px 10px">
+    <el-splitter>
+      <el-splitter-panel :resizable="false" size="40%">
+        <h1 class="text-lg font-bold text-center mt-60">Авторизация</h1>
+        <div class="p-4">
+          <el-form label-width="auto" :model="form" class="w-full flex flex-col gap-4 px-30" @submit="toAuth">
+            <el-input v-model="form.login" type="email" placeholder="example@....kz" />
+            <el-input v-model="form.password" type="password" show-password />
+            <el-button type="primary" @click="toAuth">Login</el-button>
+          </el-form>
+        </div>
+      </el-splitter-panel>
+      <el-splitter-panel :min="200">
+        <div class="demo-panel">
+          <h1>Authorization</h1>
+        </div>
+      </el-splitter-panel>
+    </el-splitter>
   </div>
 </template>
 
 <script lang="ts" setup>
+const form = reactive({
+  login: "",
+  password: "",
+});
+
 const toAuth = (e: Event) => {
   e.preventDefault();
   console.log("auth...")
 }
 </script>
 
-<style></style>
+<style scoped>
+@import "tailwindcss";
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   modules: ['@element-plus/nuxt'],
   elementPlus: {},
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ]
+  }
 })
